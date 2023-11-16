7a96b9121540b3c4021727c87ac2e745d4061ab4
Mike Patrick <mike.patrick@digital.cabinet-office.gov.uk>
2023-11-16 12:39:32 +0000

    Introduce a client for Logit's OpenSearch API
    As mentioned when installing RestClient, I did try to use the official
    OpenSearch Ruby client gem but it ultimately didn't seem to be a good
    fit for our limited use case.
    
    I've opted for basic auth API access for now purely because that's the
    access that I was able to get for myself. We might decide to switch on
    API key access and have to update how this client works before we're
    ready to start using this in production.

---
+++ lib/govuk_sli_collector/publishing_latency_sli/logit_search.rb
@@ -0,0 +1,67 @@
+require "json"
+require "rest-client"
+
+module GovukSliCollector
+  class PublishingLatencySli
+    class LogitSearch
+      class Error < StandardError; end
+
+      def initialize(host:, basic_auth:)
+        @url = URI.join(host, "_search").to_s
+        @basic_auth = basic_auth
+      end
+
+      def call(app_name:, route:, from_time:, to_time: nil, govuk_request_ids: [])
+        response = RestClient::Request.execute(
+          method: :get,
+          url:,
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Basic #{basic_auth}",
+          },
+          payload: {
+            query: {
+              bool: {
+                filter: [
+                  { term: { "kubernetes.deployment.name": app_name } },
+                  { term: { "kubernetes.container.name": "app" } },
+                  (
+                    if route.is_a?(Array)
+                      { terms: { route: } }
+                    else
+                      { term: { route: } }
+                    end
+                  ),
+                  {
+                    range: {
+                      "@timestamp": {
+                        gte: from_time.iso8601,
+                        lt: (to_time.iso8601 unless to_time.nil?),
+                      }.compact,
+                    },
+                  },
+                  (
+                    unless govuk_request_ids.empty?
+                      { terms: { govuk_request_id: govuk_request_ids } }
+                    end
+                  ),
+                ].compact,
+              },
+            },
+            fields: ["duration", "govuk_request_id", "@timestamp"],
+            _source: false,
+          }.to_json,
+        )
+
+        payload = JSON.parse(response.body)
+        payload["hits"]["hits"].map { |event_data| event_data["fields"] }
+      rescue StandardError => e
+        raise Error, e.inspect
+      end
+
+    private
+
+      attr_reader :url, :basic_auth
+    end
+  end
+end
