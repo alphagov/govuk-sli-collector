71ced0014f085038fb20c8f67ce43969edced64d
Mike Patrick <mike.patrick@digital.cabinet-office.gov.uk>
2023-12-06 17:14:18 +0000

    Put spec example in a describe
    I might merge these commits later, but attempting to minimise noise in
    the commits where I'm actually doing something

--- spec/govuk_sli_collector_spec.rb
+++ spec/govuk_sli_collector_spec.rb
@@ -1,13 +1,15 @@
 require "govuk_sli_collector"
 
 RSpec.describe GovukSliCollector do
-  it "collects the publishing latency SLI" do
-    publishing_latency_sli = instance_spy(GovukSliCollector::PublishingLatencySli)
-    allow(GovukSliCollector::PublishingLatencySli).to receive(:new)
-      .and_return(publishing_latency_sli)
+  describe "collecting the Publishing Latency SLI" do
+    it "invokes PublishingLatencySli#call" do
+      publishing_latency_sli = instance_spy(GovukSliCollector::PublishingLatencySli)
+      allow(GovukSliCollector::PublishingLatencySli).to receive(:new)
+        .and_return(publishing_latency_sli)
 
-    described_class.call
+      described_class.call
 
-    expect(publishing_latency_sli).to have_received(:call)
+      expect(publishing_latency_sli).to have_received(:call)
+    end
   end
 end
