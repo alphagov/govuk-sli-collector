298ef41633cf31ecc89a1247412cdb544227731c
Mike Patrick <mike.patrick@digital.cabinet-office.gov.uk>
2023-12-07 11:51:29 +0000

    Remove redundant namespace
    Nothing's changed, I just spotted that this isn't needed

--- lib/govuk_sli_collector.rb
+++ lib/govuk_sli_collector.rb
@@ -2,6 +2,6 @@ require "govuk_sli_collector/publishing_latency_sli"
 
 module GovukSliCollector
   def self.call
-    GovukSliCollector::PublishingLatencySli.new.call
+    PublishingLatencySli.new.call
   end
 end
